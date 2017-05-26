ea55d3342a8f6d7486777103d6ae765be346df39
Mira Ekladious <mekladious@Miras-MacBook-Pro.local>
2017-05-26 18:02:50 +0200

    components and html

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
 import { MyApp } from './app.component';
 
-import { AboutPage } from '../pages/about/about';
-import { ContactPage } from '../pages/contact/contact';
 import { HomePage } from '../pages/home/home';
+import { MessagesPage } from '../pages/messages/messages';
+import { AddPage } from '../pages/add/add';
+import { MyAccountPage } from '../pages/myaccount/myaccount';
+import { MorePage } from '../pages/more/more';
 import { TabsPage } from '../pages/tabs/tabs';
 
 import { StatusBar } from '@ionic-native/status-bar';
@@ -14,9 +16,11 @@ import { SplashScreen } from '@ionic-native/splash-screen';
 @NgModule({
   declarations: [
     MyApp,
-    AboutPage,
-    ContactPage,
     HomePage,
+    MessagesPage,
+    AddPage,
+    MyAccountPage,
+    MorePage,
     TabsPage
   ],
   imports: [
@@ -26,9 +30,11 @@ import { SplashScreen } from '@ionic-native/splash-screen';
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    AboutPage,
-    ContactPage,
     HomePage,
+    MessagesPage,
+    AddPage,
+    MyAccountPage,
+    MorePage,
     TabsPage
   ],
   providers: [
